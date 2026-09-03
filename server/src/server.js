c9db395e3b7e718047c84de169024ba07bac949a
dwainXDL <personal.dwain@gmail.com>
2026-09-03 13:36:29 +0530

    feat:connect to MongoDB before listen + health endpoint reports db state, also personal-docs to .gitignore

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,13 @@
 import { config } from "./config.js";
+import { connectDb } from "./db/connect.js";
 import app from "./app.js";
 
-app.listen(config.port, () =>
-  console.log(`API LISTENING ON http://localhost:${config.port}`),
-);
+try {
+  await connectDb();
+  app.listen(config.port, () =>
+    console.log(`API LISTENING ON http://localhost:${config.port}`),
+  );
+} catch (err) {
+  console.error("FAILED TO START - COULD NOT CONNECT TO MongoDB:", err.message);
+  process.exit(1);
+}
