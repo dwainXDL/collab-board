0e825e9010a9a897f274a512ec3858ff1bfe79c9
drnykteresteinwayne <nyktersteinshane@gmail.com>
2026-09-04 15:05:16 +0530

    feat(server): shared model options + User mongoose model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { baseOptions, toIdJSON } from "./model.options.js";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true, minlength: 2 },
+    
+    email: { type: String, required: true, trim: true, lowercase: true },
+    passwordHash: { type: String, required: true },
+  },
+  {
+    ...baseOptions,
+    toJSON: {
+      transform(doc, ret) {
+        toIdJSON(doc, ret);
+        delete ret.passwordHash;
+        return ret;
+      },
+    },
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
